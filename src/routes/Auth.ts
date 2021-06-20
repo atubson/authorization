8252f07db23c889731825eca8629a1b47e47cc8b
Atub <jakubatub@gmail.com>
2021-06-20 23:29:45 +0200

    2 Add simple form authorization

---
+++ src/routes/Auth.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import  AuthController  from '../controllers/auth';
+const auth = express.Router();
+
+auth.get('/login', AuthController.getLogin);
+
+auth.get('/signup', AuthController.getSignup);
+
+auth.post('/login', AuthController.postLogin);
+
+auth.post('/signup', AuthController.postSignup);
+
+auth.post('/logout', AuthController.postLogout);
+
+export default auth;
--- src/routes/Main.ts
+++ src/routes/Main.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import  MainController  from '../controllers/main';
+import isAuth from '../middleware/is-auth';
+
 const main = express.Router();
 
 main.get('/', MainController.getIndex);
+main.get('/protected', isAuth, MainController.getProtected);
 
 export default main;
